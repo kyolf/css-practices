683ecc8abd9d5d6e7c684bdc3fa5c22f2153c717
Kyle Szeto <kyl3sz3t0@gmail.com>
2018-01-30 15:01:56 -0800

    finish making a-b-testing with vanilla javascript

--- js/a-b-testing.js
+++ js/a-b-testing.js
@@ -1,12 +1,20 @@
 function toggleAB(){
   let toggleOff = false;
-
-  //Remove the a-b-testing class
-  if(toggleOff){
-
-  }
-  //Add the a-b-testing-class
-  else {
-
-  }
+  let bodyTag = document.getElementsByTagName('body');
+  let aBButton = document.getElementsByClassName('a-b-button');
+  aBButton[0].addEventListener('click', function() {
+    toggleOff = !toggleOff;
+    //Remove the a-b-testing class
+    if(toggleOff){
+      console.log(true);
+      bodyTag[0].classList.remove('a-b-testing');
+    }
+    //Add the a-b-testing-class
+    else {
+      console.log(false);
+      bodyTag[0].classList.add('a-b-testing');
+    }
+  });
 }
+
+toggleAB();
